d571864bbfaf1d7364cdcaf1b01aeebd41560863
Bestian Tang <bestian@gmail.com>
2023-09-28 16:00:41 +0800

    roman

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
 q-page.row.items-center.justify-evenly
-  q-img(src="../assets/ja_roman.jpg")
+  // q-img(src="../assets/ja_roman.jpg")
   
   table
     tr
-      td(v-for="phonics in phonics_a", :key="phonics")
+      th 平假名
+      th(v-for="p in phonics_a", :key="p") {{ p }}
     tr
-      td(v-for="(p, idx) in phonics_as" :key="phonics_a")
-        q-btn(@click="play(phonics_a, p, idx)", :label="getRoman(p, '')")
-
+      td a
+      td(v-for="(p, idx) in phonics_as" :key="p")
+        q-btn(@click="play(p, phonics_a, '', idx)", :label="getLabel(phonics_a, p, '', idx)")
+    tr
+      td ka
+      td(v-for="(p, idx) in phonics_ks" :key="p")
+        q-btn(@click="play(p, phonics_a, 'k', idx)", :label="getLabel(phonics_a, p, 'k', idx)")
 
 </template>
 
@@ -22,24 +27,40 @@ export default defineComponent({
   components: { ExampleComponent },
   setup () {
     return {
-      count: ref(1)
+      count: ref(1),
+      currentRoman: ref(null),
+      currentPhonics: ref(null),
+      useRoman: ref(new Array(5).fill(false))
     };
   },
   methods: {
-    getRoman (phonics: string, head: string) {
-      return head + phonics
+    getPhonics (hiragana: string) {
+      return hiragana
     },
-    play (phonics: string, p:string, idx: number) {
-      const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-      const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-
-      console.log('playing:' + p)
-      if (isOpera || isSafari) {
-        const audio = new Audio(`/phonics/${phonics[idx]}.ogg`);
+    getLabel (phonics_a:[string], hiragana: string, head: string, idx: number) {
+      console.log(hiragana)
+      if (this.currentPhonics !== hiragana) {
+        return this.getPhonics(hiragana)
       } else {
-        const audio = new Audio(`/phonics/${phonics[idx]}.mp3`);
+        let tail = phonics_a[idx]
+        console.log(tail)
+        return head + tail
       }
-      audio.play();
+    },
+    play (hiragana:string,  phonics: string, p:string, idx: number) {
+      const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+      const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === '[object SafariRemoteNotification]'; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+      this.currentPhonics = hiragana
+
+      console.log(`playing: ${p}${phonics[idx]}`)
+      if (isOpera || isSafari) {
+        const audio = new Audio(`/phonics/${p}${phonics[idx]}.ogg`);
+        audio.play();
+      } else {
+        const audio = new Audio(`/phonics/${p}${phonics[idx]}.mp3`);
+        audio.play();
+      }
     }
   }
 });
