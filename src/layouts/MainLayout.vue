a1e1222b3d86b2ff719ae3a66e095bd793bc3fa4
Bestian Tang <bestian@gmail.com>
2024-12-07 17:00:35 +0800

    完整50音

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,13 +7,17 @@ q-layout(view="lHh Lpr lFf")
       div
         a(href="https://github.com/3dw/zh-ja/", target="_blank", rel="noopener noreferrer")
           q-icon(name="palette")
-          span.thin-only 開放的教材教法 - 
+          span.thin-only 開放的教材教法 -
           span 共筆研發中
   q-drawer(v-model="leftDrawerOpen", show-if-above, bordered)
     q-list
       q-item-label(header) 學習資源
   q-page-container
-    router-view(:phonics_a="phonics_a", :phonics_song="phonics_song", :phonics_as="phonics_as", :phonics_ks="phonics_ks", :phonics_ss="phonics_ss")
+    router-view(:phonics_a="phonics_a", :phonics_song="phonics_song",
+      :phonics_as="phonics_as", :phonics_ks="phonics_ks", :phonics_ss="phonics_ss",
+      :phonics_ts="phonics_ts", :phonics_ns="phonics_ns", :phonics_hs="phonics_hs",
+      :phonics_ms="phonics_ms", :phonics_ys="phonics_ys", :phonics_ls="phonics_ls",
+      :phonics_ws="phonics_ws")
 
 </template>
 
@@ -23,54 +27,40 @@ import { defineComponent, ref } from 'vue';
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-  },
+  components: {},
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
       },
-      phonics_a: ref([
-        'a', 'i', 'u', 'e', 'o'
-      ]),
+      phonics_a: ref(['a', 'i', 'u', 'e', 'o']),
       phonics_song: ref([
-        'A', 'ka', 'sa', 'ta', 'na', 'Ha', 'ma', 'ya', 'la', 'wa'
+        'A',
+        'ka',
+        'sa',
+        'ta',
+        'na',
+        'ha',
+        'ma',
+        'ya',
+        'ra',
+        'wa',
       ]),
-      phonics_as: ref([
-        'あ', 'い', 'う', 'え', 'お'
-      ]),
-      phonics_ks: ref([
-        'か', 'き', 'く', 'け', 'こ'
-      ]),
-      phonics_ss: ref([
-        'さ', 'し', 'す', 'せ', 'そ'
-      ]),
-      phonics_ts: ref([
-        'た', 'ち', 'つ', 'て', 'と'
-      ]),
-      phonics_ns: ref([
-        'な', 'に', 'ぬ', 'ね', 'の'
-      ]),
-      phonics_hs: ref([
-        'は', 'ひ', 'ふ', 'へ', 'ほ'
-      ]),
-      phonics_ms: ref([
-        'ま', 'み', 'む', 'め', 'も'
-      ]),
-      phonics_ys: ref([
-        'や', 'ゆ', 'よ'
-      ]),
-      phonics_ls: ref([
-        'ら', 'り', 'る', 'れ', 'ろ'
-      ]),
-      phonics_ws: ref([
-        'わ', 'を', 'ん'
-      ]),
-    }
-  }
+      phonics_as: ref(['あ', 'い', 'う', 'え', 'お']),
+      phonics_ks: ref(['か', 'き', 'く', 'け', 'こ']),
+      phonics_ss: ref(['さ', 'し', 'す', 'せ', 'そ']),
+      phonics_ts: ref(['た', 'ち', 'つ', 'て', 'と']),
+      phonics_ns: ref(['な', 'に', 'ぬ', 'ね', 'の']),
+      phonics_hs: ref(['は', 'ひ', 'ふ', 'へ', 'ほ']),
+      phonics_ms: ref(['ま', 'み', 'む', 'め', 'も']),
+      phonics_ys: ref(['や', '', 'ゆ', '', 'よ']),
+      phonics_ls: ref(['ら', 'り', 'る', 'れ', 'ろ']),
+      phonics_ws: ref(['わ', '', 'ん', '', 'を']),
+    };
+  },
 });
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 q-page.row.items-center.justify-evenly
   // q-img(src="../assets/ja_roman.jpg")
-  
+
   table
     tr
       th.flex.flex-rwd
@@ -24,6 +24,48 @@ q-page.row.items-center.justify-evenly
       td(v-for="(p, idx) in phonics_ss" :key="p")
         q-btn(round, size="lg", @click="play(p, phonics_a, 's', idx)",
          :label="getLabel(phonics_a, p, 's', idx)")
+    tr
+      td ta
+      td(v-for="(p, idx) in phonics_ts" :key="p")
+        q-btn(round, size="lg", @click="play(p, phonics_a, 't', idx)",
+         :label="getLabel(phonics_a, p, 't', idx)")
+    tr
+      td na
+      td(v-for="(p, idx) in phonics_ns" :key="p")
+        q-btn(round, size="lg", @click="play(p, phonics_a, 'n', idx)",
+         :label="getLabel(phonics_a, p, 'n', idx)")
+    tr
+      td ha
+      td(v-for="(p, idx) in phonics_hs" :key="p")
+        q-btn(round, size="lg", @click="play(p, phonics_a, 'h', idx)",
+         :label="getLabel(phonics_a, p, 'h', idx)")
+    tr
+      td ma
+      td(v-for="(p, idx) in phonics_ms" :key="p")
+        q-btn(round, size="lg", @click="play(p, phonics_a, 'm', idx)",
+         :label="getLabel(phonics_a, p, 'm', idx)")
+    tr
+      td ya
+      td(v-for="(p, idx) in phonics_ys" :key="idx")
+        q-btn(v-if="p", round, size="lg", @click="play(p, phonics_a, 'y', idx)",
+         :label="getLabel(phonics_a, p, 'y', idx)")
+      td
+      td
+    tr
+      td ra
+      td(v-for="(p, idx) in phonics_ls" :key="p")
+        q-btn(round, size="lg", @click="play(p, phonics_a, 'l', idx)",
+         :label="getLabel(phonics_a, p, 'l', idx)")
+    tr
+      td wa
+      td(v-for="(p, idx) in phonics_ws" :key="idx")
+        q-btn(v-if="p", round, size="lg", @click="play(p, phonics_a, 'w', idx)",
+         :label="getLabel(phonics_a, p, 'w', idx)")
+      td
+      td
+      td
+
+
 
 </template>
 
@@ -33,43 +75,57 @@ import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  props: ['phonics_a', 'phonics_song', 'phonics_as', 'phonics_ks', 'phonics_ss'],
+  props: [
+    'phonics_a',
+    'phonics_song',
+    'phonics_as',
+    'phonics_ks',
+    'phonics_ss',
+    'phonics_ts',
+    'phonics_ns',
+    'phonics_hs',
+    'phonics_ms',
+    'phonics_ys',
+    'phonics_ls',
+    'phonics_ws',
+  ],
   components: { ExampleComponent },
-  setup () {
+  setup() {
     return {
       count: ref(1),
       currentRoman: ref(null),
       currentPhonics: ref(null),
-      useRoman: ref(new Array(5).fill(false))
+      useRoman: ref(new Array(5).fill(false)),
     };
   },
   methods: {
-    getPhonics (hiragana: string) {
-      return hiragana
+    getPhonics(hiragana: string) {
+      return hiragana;
     },
-    getLabel (phonics_a:[string], hiragana: string, head: string, idx: number) {
-      console.log(hiragana)
+    getLabel(phonics_a: [string], hiragana: string, head: string, idx: number) {
       if (this.currentPhonics !== hiragana) {
-        return this.getPhonics(hiragana)
+        return this.getPhonics(hiragana);
       } else {
-        let tail = phonics_a[idx]
+        let tail = phonics_a[idx];
         if (head === 's' && tail === 'i') {
-          head = 'sh'
+          head = 'sh';
+        } else if (head === 't' && tail === 'i') {
+          head = 'ch';
+        } else if (head === 't' && tail === 'u') {
+          head = 'ts';
         }
-        console.log(tail)
-        return head + tail
+        return head + tail;
       }
     },
-    play (hiragana:string, phonics: string, p:string, idx: number) {
-      this.currentPhonics = hiragana
-      
-      const text = p + phonics[idx]
-      const utterance = new SpeechSynthesisUtterance(text)
-      utterance.lang = 'ja-JP'
-      utterance.rate = 0.8 // 調整速度，可以根據需要修改
-      
-      window.speechSynthesis.speak(utterance)
-    }
-  }
+    play(hiragana: string, phonics: string, p: string, idx: number) {
+      this.currentPhonics = hiragana;
+
+      const utterance = new SpeechSynthesisUtterance(hiragana);
+      utterance.lang = 'ja-JP';
+      utterance.rate = 0.8;
+
+      window.speechSynthesis.speak(utterance);
+    },
+  },
 });
 </script>
